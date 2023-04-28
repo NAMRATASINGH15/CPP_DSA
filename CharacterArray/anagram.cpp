e6b70808fc9353b5589d15c0ae29ba4a9939f3e3
NAMRATASINGH15 <namratasinghvns7@gmail.com>
2023-04-29 00:32:22 +0530

    Valid Anagram

---
+++ CharacterArray/anagram.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+bool isAnagram(string s, string t) {
+  if (s.length() != t.length()) {
+    return false;
+  }
+        
+  vector<int> freq(26, 0);
+    for (int i = 0; i < s.length(); i++) {
+      freq[s[i] - 'a']++;
+      freq[t[i] - 'a']--;
+    }
+        
+    for (int i = 0; i < freq.size(); i++) {
+      if (freq[i] != 0) {
+        return false;
+      }
+    }
+        
+  return true;
+}
+
+int main() {
+  string s = "anagram";
+  string t = "nagaram";
+
+  bool ans = isAnagram(s, t);
+  cout << ans << endl;
+}
